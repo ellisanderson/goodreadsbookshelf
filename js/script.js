02eb589f24198c210940fa5cbc3ea7a2a4129952
ellisanderson <ellishenryanderson@gmail.com>
2017-03-26 19:51:04 -0400

    Initial markup of GoodReads redesign project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,56 @@
 $(document).ready(function(){
-  $('.dropdown-menu').hide();
   $('.hamburger').click(function(){
-    $('.dropdown-menu').slideToggle('slow');
+    $('.hamburger-dropdown-menu').slideToggle('slow');
+  })
+});
+
+$(document).ready(function(){
+  $('.ham-browse').click(function(){
+    $('.ham-community-menu').slideUp('slow');
+    $('.ham-account-menu').slideUp('slow');
+    $('.ham-browse-menu').slideToggle('slow');
+  })
+});
+
+$(document).ready(function(){
+  $('.ham-community').click(function(){
+    $('.ham-browse-menu').slideUp('slow');
+    $('.ham-account-menu').slideUp('slow');
+    $('.ham-community-menu').slideToggle('slow');
+  })
+});
+
+$(document).ready(function(){
+  $('.ham-account').click(function(){
+    $('.ham-browse-menu').slideUp('slow');
+    $('.ham-community-menu').slideUp('slow');
+    $('.ham-account-menu').slideToggle('slow');
+  })
+});
+
+
+
+
+
+// SLICK CAROUSEL SLIDER jQuery
+$(document).ready(function(){
+  $('.hero').slick({
+    // autoplay:true,
+    autoplaySpeed:10000,
+    arrows:true,
+    dots:true,
+    speed:1000
+  });
+});
+
+
+
+
+
+
+// BOOKSHELF SORTING jQuery
+$(document).ready(function(){
+  $('#sort-have-read').change(function(){
+    $('#all').hide();
   })
 });
